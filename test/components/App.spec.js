67276224217421892bc971a391636983a78d0e35
lil <li@last.nl>
2017-11-13 13:31:05 +0100

    Test test files move to test dir
    For the moment tests are of little use.

--- test/components/App.spec.js
+++ test/components/App.spec.js
@@ -5,8 +5,9 @@ import App from './App'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
-  const tree = renderer.create(
+test('renders correctly', () => {
+  // const tree =
+  renderer.create(
     <App />
   )
 })

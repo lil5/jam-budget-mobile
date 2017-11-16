03b7692d6e2ae27fc52f075d94f767071be2c320
lil <li@last.nl>
2017-11-16 16:33:12 +0100

    Change Drawer list from ReactNavigation to RNMaterialUI

--- app/components/DrawerCustom.js
+++ app/components/DrawerCustom.js
@@ -7,13 +7,44 @@ export default class DrawerCustom extends Component {
   static contextTypes = {
     uiTheme: PropTypes.object.isRequired,
   }
+
+  // https://github.com/react-community/react-navigation/blob/e25bdd2ed2fcee5e9e811ab225a3741ec7b75ca3/src/views/Drawer/DrawerNavigatorItems.js#L18
   static propTypes = {
-    props: PropTypes.object.isRequired,
+    navigation: PropTypes.object.isRequired, // NavigationScreenProp<NavigationState>,
+    items: PropTypes.array.isRequired, // Array<NavigationRoute>,
+    activeItemKey: PropTypes.string,
+    activeTintColor: PropTypes.string,
+    activeBackgroundColor: PropTypes.string,
+    inactiveTintColor: PropTypes.string,
+    inactiveBackgroundColor: PropTypes.string,
+    getLabel: PropTypes.func.isRequired, // scene: DrawerScene) => ?(React.Node | string),
+    renderIcon: PropTypes.func.isRequired, // scene: DrawerScene) => ?React.Node,
+    onItemPress: PropTypes.func.isRequired, // info: DrawerItem) => void,
+    itemsContainerForceInset: PropTypes.object,
+    itemsContainerStyle: PropTypes.object, // ViewStyleProp,
+    itemStyle: PropTypes.object, // ViewStyleProp,
+    labelStyle: PropTypes.object, // TextStyleProp,
+    iconContainerStyle: PropTypes.object, // ViewStyleProp,
+    drawerPosition: PropTypes.oneOf(['left' | 'right']).isRequired,
+  }
+
+  onItemPress (route, focused) {
+    const { navigation } = this.props
+
+    navigation.navigate('DrawerClose')
+    navigation.navigate(route.routeName)
   }
 
   render () {
-    const { props } = this.props
+    const {
+      items,
+      activeItemKey,
+      getLabel,
+      renderIcon,
+    } = this.props
+
     const { palette, drawerSectionActiveItem } = this.context.uiTheme
+
     return (
       <Drawer>
         <Drawer.Header>
@@ -33,7 +64,7 @@ export default class DrawerCustom extends Component {
             }}
           />
         </Drawer.Header>
-        <DrawerItems {...props}
+        <DrawerItems {...this.props}
           activeTintColor={palette.primaryColor}
           activeBackgroundColor={drawerSectionActiveItem.container.backgroundColor}
           inactiveTintColor={palette.primaryTextColor}
@@ -42,15 +73,20 @@ export default class DrawerCustom extends Component {
         />
         <Drawer.Section
           divider
-          items={[
-            { icon: 'developer-board',
-              value: 'Testing',
-              onPress: () => {
-                props.navigation.navigate('Home')
-              }},
-            { icon: 'today', value: 'Calendar', active: true },
-            { icon: 'people', value: 'Clients' },
-          ]}
+          items={items.map((route, index) => {
+            const focused = activeItemKey === route.key
+            const tintColor = focused ? palette.primaryColor : palette.secondaryTextColor
+            const scene = { route, index, focused, tintColor }
+            const icon = renderIcon(scene)
+            const label = getLabel(scene)
+            return {
+              active: focused,
+              value: label,
+              icon,
+              key: route.key,
+              onPress: () => this.onItemPress(route, focused),
+            }
+          })}
         />
         <Drawer.Section
           title='Personal'

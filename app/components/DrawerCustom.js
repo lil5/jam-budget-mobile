851587f406eadbcaa62afb667d62d2776042c796
lil <li@last.nl>
2017-11-18 11:10:56 +0100

    Fix warning drawerPosition is undefined

--- app/components/DrawerCustom.js
+++ app/components/DrawerCustom.js
@@ -25,7 +25,7 @@ export default class DrawerCustom extends Component {
     itemStyle: PropTypes.object, // ViewStyleProp,
     labelStyle: PropTypes.object, // TextStyleProp,
     iconContainerStyle: PropTypes.object, // ViewStyleProp,
-    drawerPosition: PropTypes.oneOf(['left' | 'right']).isRequired,
+    drawerPosition: PropTypes.oneOf(['left' | 'right']),
   }
 
   onItemPress (route, focused) {

83fa5dc46b26bac2e996fd7766837ec23f0926c8
lil <li@last.nl>
2017-11-20 11:09:51 +0100

    Add store to index (not working!)

--- app/index.js
+++ app/index.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
 import { StatusBar } from 'react-native'
 
+// themes
 import { ThemeProvider } from 'react-native-material-ui'
 import Theme from './styles/Theme'
 
+// rn navigator
 import { RootNavigator } from './routes'
+
+// redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 export default class extends Component {
   render () {
     return (
-      <ThemeProvider uiTheme={Theme}>
-        <StatusBar
-          backgroundColor={Theme.palette.primaryColor}
-          barStyle='light-content'
-        />
-        <RootNavigator />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider uiTheme={Theme}>
+          <StatusBar
+            backgroundColor={Theme.palette.primaryColor}
+            barStyle='light-content'
+          />
+          <RootNavigator />
+        </ThemeProvider>
+      </Provider>
     )
   }
 }

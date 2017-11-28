fc4c812d94c1aecaec532a0a8f9dc599f8d04212
lil <li@last.nl>
2017-11-28 22:57:43 +0100

    Remove getEnvelopes and Change envelopesReducer defaultState

--- app/actions/envelopes.js
+++ app/actions/envelopes.js
@@ -21,26 +21,3 @@ export function deleteEnvelope (id) {
     }, 300),
   }
 }
-
-export function getEnvelopes () {
-  const test = {
-    data: [
-      { title: 'Food', amount: 400, catKey: 0, key: 0 },
-      { title: 'Fun', amount: -40, catKey: 0, key: 1 },
-      { title: 'Clothes', amount: 30, catKey: 0, key: 2 },
-      { title: 'Rent', amount: 400, catKey: 1, key: 3 },
-    ],
-    catagories: [
-      'Shopping', 'Living Expences',
-    ],
-  }
-
-  return {
-    type: 'GET_ENVELOPES',
-    payload: new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(test)
-      }, 2e3)
-    }),
-  }
-}

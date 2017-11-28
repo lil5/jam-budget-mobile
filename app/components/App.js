990c2b27b7eec580b4f4390d0df65ba34e3e84ce
lil <li@last.nl>
2017-11-28 12:15:57 +0100

    Add state counter

--- app/components/App.js
+++ app/components/App.js
@@ -3,12 +3,25 @@ import {
   StyleSheet,
   Text,
   View,
+  Button,
 } from 'react-native'
+// import PouchDB from 'pouchdb-react-native'
+// const localDB = new PouchDB('myDB')
 
 const instructions = 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu'
 
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentWillMount () {
+    this.setState({
+      counter: 1,
+    })
+  }
   render () {
     return (
       <View style={styles.container}>
@@ -18,11 +31,28 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           To get started, edit "app/compontents/App.js".
         </Text>
+        <Button onPress={() => this.onPressBtnAdd()} title='add' />
+        <Button onPress={() => this.onPressBtnRemove()} title='remove' />
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Text style={styles.instructions}>
+          {this.state.counter}
+        </Text>
       </View>
     )
+  }
+
+  onPressBtnAdd () {
+    this.setState({
+      counter: ++this.state.counter,
+    })
+  }
+
+  onPressBtnRemove () {
+    this.setState({
+      counter: --this.state.counter,
+    })
   }
 }
 

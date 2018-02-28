c7d66ac65d101183fa6aa43bcc0f47e2babb7556
Lucian I. Last <lil5@disroot.org>
2018-02-28 08:55:57 +0100

    Code style change Settings var abstraction
    Stage hunk: not tested!

--- app/pages/Settings.js
+++ app/pages/Settings.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { updateDefaultCurrency } from '../redux/actions'
 import PropTypes from 'prop-types'
-import { SectionList } from 'react-native'
 import SelectCurrency from '../components/SelectCurrency'
 import Footer from '../components/Footer'
 import * as NB from 'native-base'
@@ -33,6 +32,8 @@ class Settings extends Component {
 
   render () {
     const { history } = this.context.router
+    const { defaultCurrency, updateDefaultCurrency } = this.props
+
     return (
       <NB.Container>
         <NB.Header>
@@ -46,8 +47,8 @@ class Settings extends Component {
           </NB.Separator>
           <NB.Form>
             <SelectCurrency
-              defaultValue={this.props.defaultCurrency}
-              onChangeText={value => this.props.updateDefaultCurrency(value)}
+              defaultValue={defaultCurrency}
+              onChangeText={value => updateDefaultCurrency(value)}
             />
           </NB.Form>
         </NB.Content>

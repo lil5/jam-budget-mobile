b4ee3440a3d342bb2e9a92c4f4e2b3711d5a9e5f
Lucian I. Last <li@last.nl>
2018-10-01 12:08:27 +0200

    Working example
    Uses redux-remember

--- app/store.js
+++ app/store.js
@@ -1,8 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { applyMiddleware } from 'redux'
+import { AsyncStorage } from 'react-native'
+import reduxRemember from 'redux-remember'
 import { logger } from 'redux-logger'
 import thunk from 'redux-thunk'
 
+// reducers
 import counterReducer from './reducers/counterReducer'
+
+const { createStore, combineReducers } = reduxRemember(AsyncStorage)
 
 export default createStore(
   combineReducers({
